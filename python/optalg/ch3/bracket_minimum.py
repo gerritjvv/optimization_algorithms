4bf5ef325e1ec4dee17a8b3a2c4d0001018e5d0b
gerritjvv <gerritjvv@gmail.com>
2022-01-28 11:59:49 +0100

    update structure for python code

---
+++ python/optalg/ch3/bracket_minimum.py
@@ -0,0 +1,5 @@
+# From algorithm 3.1
+# Bracketing is a type of algorithm that tries to guess the range in which a minimum
+# or several minimum's can fall.
+# We end up with 3 points, the range [a,b] and a point c that is f(a) < f(c) < f(b)
+
